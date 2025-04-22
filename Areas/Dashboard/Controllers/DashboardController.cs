42dcaefc5156aeffac88e72405d60a453b03791b
Kayacan <kirpiklikayacan@gmail.com>
2025-04-22 18:25:13 +0300

    dash init

---
+++ Areas/Dashboard/Controllers/DashboardController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BirileriWebSitesi.Areas.Dashboard.Controllers
+{
+    [Area("Dashboard")]
+    [Route("Dashboard/[controller]/[action]")]
+    [Authorize(Roles = "Admin")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
