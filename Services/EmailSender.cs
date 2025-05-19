c0fb4dacd8369fd8918ac6525ed7e3c2e7e9d595
kayacankaya <kirpiklikayacan@gmail.com>
2025-05-19 14:35:31 +0300

    email configuration made

---
+++ Services/EmailSender.cs
@@ -0,0 +1,92 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using BirileriWebSitesi.Interfaces;
+
+namespace BirileriWebSitesi.Services
+{
+    public class SmtpEmailSender : IEmailSender
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<SmtpEmailSender> _logger;
+        public SmtpEmailSender(IConfiguration configuration, ILogger<SmtpEmailSender> logger)
+        {
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        public async Task<bool> SendEmailAsync(string email, string subject,string from, string type)
+        {
+            try
+            {
+                //using var client = new SmtpClient();
+                //client.Host = _configuration["Smtp:Host"];
+                //client.Port = int.Parse(_configuration["Smtp:Port"]);
+                //client.Credentials = new NetworkCredential(
+                //    _configuration["Smtp:Username"],
+                //    _configuration["Smtp:Password"]);
+                //client.EnableSsl = true;
+
+                //var message = new MailMessage
+                //{
+                //    From = new MailAddress(from),
+                //    Subject = subject,
+                //    Body = subject,
+                //    IsBodyHtml = true,
+                //};
+                //message.To.Add(email);
+
+                //await client.SendMailAsync(message);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message.ToString());
+                return false;   
+            }
+            
+        }
+        public async Task<bool> SendContactUsEmailAsync(string username, string email,string? phone, string message, string? subject)
+        {
+            try
+            {
+
+                string phoneNumber = phone ?? string.Empty;
+                string subjectString = subject ?? string.Empty;
+
+
+                var mimeMessage = new MimeMessage();
+                mimeMessage.From.Add(new MailboxAddress("Birileri", _configuration["SMTP:Username"]));
+                mimeMessage.To.Add(MailboxAddress.Parse(_configuration["SMTP:InfoAddress"]));
+                mimeMessage.Cc.Add(MailboxAddress.Parse(_configuration["SMTP:CC1"]));
+                mimeMessage.Cc.Add(MailboxAddress.Parse(_configuration["SMTP:CC2"]));
+                mimeMessage.Subject = subjectString ?? "";
+
+                string htmlMessage = $"Birileri Girişimci Takımı'ndan gelen iletişim formu mesajı:<br><br>" +
+                    $"<strong>Kullanıcı İsmi:</strong> {username}<br>" +
+                    $"<strong>Gönderen:</strong> {email}<br>" +
+                    $"<strong>Telefon:</strong> {phoneNumber}<br>" +
+                    $"<strong>Konu:</strong> {subject}<br>" +
+                    $"<strong>Mesaj:</strong> {message}<br>";
+
+                mimeMessage.Body = new TextPart("html") { Text = htmlMessage };
+
+                using var smtp = new SmtpClient();
+                await smtp.ConnectAsync(_configuration["SMTP:Host"], 465, MailKit.Security.SecureSocketOptions.SslOnConnect);
+                await smtp.AuthenticateAsync(_configuration["SMTP:Username"], _configuration["SMTP:Password"]);
+                await smtp.SendAsync(mimeMessage);
+                await smtp.DisconnectAsync(true);
+
+                return true;
+
+                
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message.ToString());
+                return false;
+            }
+            
+        }
+    }
+}
