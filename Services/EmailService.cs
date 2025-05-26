ee60de33aa6548e29135aba836ac6697e89a2475
kayacancankaya <157284231+kayacancankaya@users.noreply.github.com>
2025-05-26 11:17:15 +0300

    payment email

--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -44,6 +44,53 @@ namespace BirileriWebSitesi.Services
             }
             
         }
+        public async Task<bool> SendPaymentEmailAsync(string to, int orderID, string paymentType)
+        {
+            try
+            {
+                var email = new MimeMessage();
+                email.From.Add(new MailboxAddress("Birileri", _configuration["SMTP:Username"]));
+                email.To.Add(MailboxAddress.Parse(to));
+                string htmlMessage = string.Empty;
+                if(paymentType="CreditCard")
+                {
+                    string subject = String.Format("{0} - Ödeme Alındı",orderID)
+                    email.Subject = subject;
+                    htmlMessage = String.Format("{0} Sipariş Numarasına ait ödemeniz başarıyla alınmıştır. 
+                                                    Siparişiniz en geç 2 iş günü içerisinde hazırlanarak
+                                                    kargo firmasına (Aras Kargo) teslim edilecektir. Siparişinizin durumunu
+                                                    <a href="https://birilerigt.com/Identity/Account/Manage"> buradan </a>
+                                                    takip edebilirsiniz.",orderID)
+                }
+                else
+                {
+                    string subject = String.Format("{0} - Hesaba Havale",orderID)
+                    email.Subject = subject;   
+                    htmlMessage = String.Format("{0} Siparişiniz alınmıştır. 
+                                                    Banka havale formunu
+                                                    <a href="https://birilerigt.com/Identity/Account/Manage"> buradan </a>
+                                                    gönderdiğinizde siparişiniz işleme alınacak, en geç 2 iş günü içerisinde
+                                                    kargo firmasına (Aras Kargo) teslim edilecektir.",orderID)
+                }
+
+                var builder = new BodyBuilder { HtmlBody = htmlMessage };
+                email.Body = builder.ToMessageBody();
+
+                using var smtp = new SmtpClient();
+                smtp.Timeout = 10000; // Timeout in milliseconds (10 seconds)
+                await smtp.ConnectAsync("mail.kurumsaleposta.com", 465, SecureSocketOptions.SslOnConnect);
+                await smtp.AuthenticateAsync(_configuration["SMTP:Username"], _configuration["SMTP:Password"]);
+                await smtp.SendAsync(email);
+                await smtp.DisconnectAsync(true);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message.ToString());
+                return false;   
+            }
+            
+        }
         public async Task<string> SendContactUsEmailAsync(string username, string email,string? phone, string message, string? subject)
         {
             try
