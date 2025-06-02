f1fc729fe083c394d66fdf99a2048284a3481858
kayacancankaya <157284231+kayacancankaya@users.noreply.github.com>
2025-06-02 09:08:19 +0300

    Update ProductDetailedViewModel.cs

--- Models/ViewModels/ProductDetailedViewModel.cs
+++ Models/ViewModels/ProductDetailedViewModel.cs
@@ -7,6 +7,5 @@
         public Dictionary<string, string> VariantAttributes { get; set; } = new();
         public ProductDetailedVariantInfoViewModel ProductVariantInfo { get; set; } = new();
         public ProductDetailedVariantImageViewModel ProductVariantImage { get; set; } = new();
-        public int Quantity { get; set; } = 0;
     }
 }
