44bf0c34fd1eb416c6a1394a53b176acec4bee3e
kayacancankaya <157284231+kayacancankaya@users.noreply.github.com>
2025-06-04 15:01:36 +0300

    Update Login.cshtml.cs

--- Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -170,7 +170,7 @@ namespace BirileriWebSitesi.Areas.Identity.Pages.Account
                         await _basketService.TransferInquiryBasketAsync(inquiry, user.Id);
                         HttpContext.Response.Cookies.Delete("MyInquiry");
                     }
-                    return RedirectToPage("/Identity/Account/Manage/Index");
+                    return Redirect("/Identity/Account/Manage/Index");
 
                 }
                 if (result.RequiresTwoFactor)
